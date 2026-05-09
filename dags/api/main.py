dd37499a368f7529efe1fa2325bc00ebdcb37cf7
blueoxygens <exboxer234@gmail.com>
2026-05-09 19:02:08 +0900

    feat: dag

---
+++ dags/api/main.py
@@ -0,0 +1,60 @@
+from airflow import DAG
+import pedulum
+from datetime import datetime, timedelta
+from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
+
+local_tz = pendulum.timezone("Asia/Seoul")
+
+
+from datetime import datetime, timedelta
+
+default_args = {
+    # DAG 소유자 (담당자 이름)
+    "owner": "khj",
+    
+    # 이메일 알림 받을 주소
+    # "email": ["your_email@example.com"],
+    
+    # 실패 시 이메일 알림 여부
+    # "email_on_failure": False,
+    
+    # 재시도 시 이메일 알림 여부
+    # "email_on_retry": False,
+    
+    # 실패 시 재시도 횟수
+    # "retries": 1,
+    
+    # 재시도 간격 (5분)
+    # "retry_delay": timedelta(minutes=5),
+    
+    # DAG 시작 날짜 (이 날짜부터 스케줄링 시작)
+    "start_date": datetime(2026, 1, 1),
+    
+    # 과거 실행 건 자동 실행 여부
+    "catchup": False,
+    
+    # Task 최대 실행 시간 (타임아웃)
+    "execution_timeout": timedelta(hours=1),
+    
+    # 이전 DAG Run이 완료될 때까지 대기
+    "depends_on_past": False,
+    
+    # Task 실행 대기 시간 제한
+    "dagrun_timeout": timedelta(hours=2),
+}
+
+with DAG(
+    dag_id = "produce_json",
+    default_args = default_args,
+    description = 'DAG to produce JSON fine with raw data',
+    schedule = '0 14 * * *',
+) as dag:
+    
+    #define tasks
+    playlist_id = get_playlist_id()
+    video_ids =  get_video_ids()
+    extract_data = extract_video_data()
+    save_to_json_task = save_to_json()
+
+    #define dependencies
+    playlist_id >> video_ids >> extract_data >> save_to_json_task
--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,17 +2,16 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+from airflow.decorators import task
+from airflow.models import Variable
 
-load_dotenv(dotenv_path="./.env")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 MAX_RESULTS = 50
 
 
-
+@task
 def get_playlist_id():
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
@@ -27,7 +26,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def get_video_ids(playlistid):
 
     video_ids = []
@@ -64,7 +63,7 @@ def get_video_ids(playlistid):
 
 
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -107,6 +106,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
